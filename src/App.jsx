86579edbad81b705473e29642df61f54a6ed65aa
sujal-pawar <sujalpawar00007@gmail.com>
2025-02-16 12:48:05 +0530

    Added locomotion

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,53 @@
-
-import { useState } from 'react'
+import { useEffect, useRef } from "react";
+import LocomotiveScroll from "locomotive-scroll";
 import React from "react";
-import './App.css'
-import './index.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+import './App.css';
+import './index.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 import About from './components/About';
 import Event from './components/Event';
 import Sponsors from './components/Sponsors';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll({
+      el: scrollRef.current,
+      smooth: true,
+    });
+
+    return () => {
+      locomotiveScroll.destroy();
+    };
+  }, []);
 
   return (
-    <>
-      <div className="bg-black w-full fixed top-0 h-18 z-50 flex items-center ">
+    <div ref={scrollRef} data-scroll-container>
+      <div className="bg-black w-full fixed top-0 h-18 z-50 flex items-center">
         <Navbar />
       </div>
-      <section className="flex flex-col backdrop-blur-xl items-center justify-center h-screen w-full"> 
+
+      <section data-scroll-section className="flex flex-col backdrop-blur-xl items-center justify-center h-screen w-full bg-cover bg-center" >
         <Hero />
       </section>
-      <section className='flex bg-black text-white flex-col items-center justify-center h-screen w-full'>
-        <About/>
+
+      <section data-scroll-section 
+      className='flex bg-black text-white flex-col items-center justify-center h-screen w-full'
+      style={{ backgroundImage: "url('')" }}>
+        <About />
       </section>
-      <section>
-        <Event/>
+
+      <section data-scroll-section>
+        <Event />
       </section>
-      <section>
-      <Sponsors/>
+
+      <section data-scroll-section>
+        <Sponsors />
       </section>
-      
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
