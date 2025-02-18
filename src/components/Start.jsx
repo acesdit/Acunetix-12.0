1c6054fbdb9445fbb11b27dcb492d127e29f5c08
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-02-19 02:05:32 +0530

    background animation

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -20,11 +20,8 @@ export default function LogoAnimation() {
   }, []);
 
   return (
-
     <div className={`fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black overflow-hidden transition-opacity duration-1000 ${fadeOut ? "opacity-0" : "opacity-100"}`}>
-      {/* Content Container */}
       <div className={`relative flex flex-col items-center z-10 transition-all duration-1000 ${moveUp ? "mt-[-40vh]" : "mt-0"}`}>
-        {/* Logo with scaling and translation */}
         <img
           src={img}
           alt="Logo"
@@ -33,8 +30,18 @@ export default function LogoAnimation() {
           }`}
         />
 
-        
-
+        {showText && (
+          <div className="flex flex-col items-center mt-8 space-y-4 translate-y-[-10vh]">
+            <h1 className="text-4xl md:text-6xl font-extrabold uppercase text-center mx-auto">
+              <span className="shiny-text bg-gradient-to-r from-gray-500 via-gray-300 to-gray-200 bg-clip-text text-transparent">
+                Acunetix 12
+              </span>
+            </h1>
+            <p className="text-gray-300 text-sm md:text-lg text-center animate-subtext-fade">
+              DIVE INTO THE WORLD OF ILLUSIONS
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
