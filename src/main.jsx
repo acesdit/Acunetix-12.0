a65845af0f32e1ddb09ced8f16e0446d349899d2
Prasadkandekar <prasadkandekar2@gmail.com>
2025-02-20 15:05:16 +0530

    Event Pages created

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import React from "react";
 import { BrowserRouter } from 'react-router-dom';
+import CodeOfLies from './pages/CodeOfLies.jsx';
+import {Brainiac} from "./pages/Brainiac.jsx"
+import { Dpl } from './pages/Dpl.jsx';
+import { TreasureTrove } from './pages/TreasureTrove.jsx';
+import { Timescape } from './pages/TimeScape.jsx';
+import { GameStorm } from './pages/GameStorm.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+  <GameStorm/>
+    {/* <App /> */}
   </BrowserRouter>,
 )
