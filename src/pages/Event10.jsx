8d78b9b5082d07dc2122f9c55112c03b6b68318b
Prasadkandekar <prasadkandekar2@gmail.com>
2025-02-20 10:31:49 +0530

    event pages added

---
+++ src/pages/Event10.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Event10 = () => {
+  return (
+    <div>Event 10 Dummy pages</div>
+  )
+}
+
+export default Event10
---
+++ src/pages/Event9.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Event9 = () => {
+  return (
+    <div>Event 9  dummy page</div>
+  )
+}
+
+export default Event9
