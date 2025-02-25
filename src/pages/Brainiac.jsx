de1bd741539995aa8683736395d6fffbe4be95de
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-02-25 11:50:58 +0530

    select

--- src/pages/Brainiac.jsx
+++ src/pages/Brainiac.jsx
@@ -65,7 +65,7 @@ const Brainiac = () => {
         <IoArrowBack className="text-lg" />
       </button>
 
-      <div className="relative min-h-screen pt-24 pb-12">
+      <div className="relative min-h-screen pt-24 ">
         <div className="absolute inset-0">
           <LetterGlitch
             glitchSpeed={50}
@@ -73,11 +73,11 @@ const Brainiac = () => {
             outerVignette={false}
             smooth={true}
           />
-          <div className="absolute inset-0 bg-black/60"></div>
+          <div className="absolute inset-0 bg-black/60 pb-0 "></div>
         </div>
 
         {/* Content Container */}
-          <div className="relative z-10 container mx-auto px-4 py-12 h-full sm:h-[50vh] md:h-[70vh]">
+          <div className="relative z-10 container mx-auto px-4 py-12 h-full sm:h-[50vh] md:h-[70vh] pb-0  ">
             <div className="flex flex-col md:flex-row items-center gap-8 h-full">
               {/* Left Column */}
             <div className="w-full md:w-1/2 flex flex-col justify-center items-center text-center h-full space-y-10">
@@ -88,9 +88,9 @@ const Brainiac = () => {
                       <img
                         src={brainiacImg}
                         alt="Event Poster"
-                        className="rounded-xl shadow-2xl w-full max-w-sm max-h-[350px] object-contain transform hover:scale-105 transition-all duration-300"
+                        className="rounded-xl shadow-2xl w-full max-w-sm max-h-[350px] pb-5 object-contain transform hover:scale-105 transition-all duration-300"
                       />
-                      <div className="bg-black/30 backdrop-blur-sm rounded-2xl border border-gray-500 p-6 shadow-xl hover:shadow-2xl transition-all duration-300 w-full max-w-sm">
+                      <div className="bg-black/30 backdrop-blur-sm rounded-2xl border border-gray-500 p-6 shadow-xl hover:shadow-2xl transition-all duration-300 w-full max-w-sm mt-5">
                         <p className="text-gray-50 text-sm leading-relaxed">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
