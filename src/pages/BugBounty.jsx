716c3e4bc3aed7aade7612610b2611db04467c74
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-03-05 14:25:19 +0530

    fg

--- src/pages/BugBounty.jsx
+++ src/pages/BugBounty.jsx
@@ -81,7 +81,7 @@ const BugBounty = () => {
             <div className="flex flex-col md:flex-row items-center gap-8 h-full">
               {/* Left Column */}
             <div className="w-full md:w-1/2 flex flex-col justify-center items-center text-center h-full space-y-10">
-              <h1 className="text-5xl md:text-6xl font-bold text-white">Brainiac</h1>
+              <h1 className="text-5xl md:text-6xl font-bold text-white">Bug Bounty</h1>
 
               {/* Mobile View */}
                       <div className="md:hidden flex flex-col items-center space-y-10" >
