3efc480369738d6c6275d4b8ab1a1758fc3d00cf
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-03-06 13:07:23 +0530

    event added

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -63,13 +63,13 @@ import Brainiac from "../assets/Brainiac.webp"
     description: "First Event",
     link: "/event/TreasureTrove",
   },
-  {
-    id: "GameStorm",
-    image: GameStorm,
-    title: "GameStorm",
-    description: "First Event",
-    link: "/event/gameStorm",
-  },
+  // {
+  //   id: "GameStorm",
+  //   image: GameStorm,
+  //   title: "GameStorm",
+  //   description: "First Event",
+  //   link: "/event/gameStorm",
+  // },
   {
     id: "Dpl",
     image: Dpl,
