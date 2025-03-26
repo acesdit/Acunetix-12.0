50ed61a84220d6ee6db3602b43d25cf449bd307b
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-03-26 12:53:27 +0530

    video change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import video from '../assets/Background.mp4';
+import video from '../assets/Background1.mp4';
 
 const Hero = () => {
     const [timeLeft, setTimeLeft] = useState({
